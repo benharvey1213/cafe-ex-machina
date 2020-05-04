8692726236792f924b03b38076b1248bfe2f1bcd
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-05-03 21:09:06 -0500

    added screens and basic layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaymentComponent } from './payment/payment.component';
+import { StatusComponent } from './status/status.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'payment', component: PaymentComponent},
+  {path: 'status', component: StatusComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
