916fcad2ce0a70bb15b443dd2ed055e39a723c71
JobeFarkley <dalin.christianson@yahoo.com>
2020-05-04 14:45:46 -0500

    Added some functionality

--- src/app/order-list-page/order-list-page.component.ts
+++ src/app/order-list-page/order-list-page.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list-page.component.css']
 })
 export class OrderListPageComponent implements OnInit {
-
+  headers = ["Name", "Price"];
+  rows = [
+    {
+      "Name": "Red Bean Soup",
+      "Price": 10.50
+    },
+    {
+      "Name": "black coffee",
+      "Price": 9.5
+    },
+    {
+      "Name": "surgar cookie",
+      "Price": 5
+    }];
   constructor() { }
 
   ngOnInit(): void {
