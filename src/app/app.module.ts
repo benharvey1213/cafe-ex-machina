d241561b7a4f1625cfc65bb6b2e74cf98f274f08
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-05-06 20:31:21 -0500

    added persistent header and running total in data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { CategoryComponent } from './category/category.component';
 import { OrderListPageComponent } from './order-list-page/order-list-page.component';
 import { PaymentComponent } from './payment/payment.component';
 import { StatusComponent } from './status/status.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { StatusComponent } from './status/status.component';
     CategoryComponent,
     OrderListPageComponent,
     PaymentComponent,
-    StatusComponent
+    StatusComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
