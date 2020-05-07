cc18015e865db22e6891a6d7e73eef50e18b4bbf
Andy H <fan.horror@gmail.com>
2020-05-06 22:14:16 -0500

    Added menu
    Added mock-menu.ts
    Added arguments for a product item in product.ts
    Added getTotal() and getCart() methods to the data-interaction.service.ts file

--- src/app/data-interaction.service.ts
+++ src/app/data-interaction.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product';
+import { MENU } from './mock-menu'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataInteractionService {
-
-  inventoryList : Product[
-
-  ];
+  menu = MENU;
 
   currentShoppingCart : Product[];
   currentTotal : number = 0;
+
+  getTotal(): number{
+    let total = 0;
+    for(let i=0; i < this.currentTotal; i++)
+      this.currentShoppingCart[i].price += total;
+    return total; //TODO: format total as a $ amount will return a string instead of number
+  }
+
+  getCart(): Product[]{
+    return this.currentShoppingCart;
+  }
 
   constructor() { }
 
--- src/app/product.ts
+++ src/app/product.ts
@@ -3,4 +3,6 @@ export class Product {
     description : string;
     price : number;
     imageUrl : string;
+    category : string;
+    availability: number; //1: morning (7am - 11:30am) //2: lunch (11:00am - close) //3: all day
 }
