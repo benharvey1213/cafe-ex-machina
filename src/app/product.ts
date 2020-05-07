dd5cc7060864d0094afcf934346cf3c4fae97dd0
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-05-06 20:54:01 -0500

    product includes category now

--- src/app/product.ts
+++ src/app/product.ts
@@ -2,5 +2,15 @@ export class Product {
     name : string;
     description : string;
     price : number;
+    category : string;
     imageUrl : string;
+
+    constructor(name : string, description : string, price : number, category : string, imageUrl : string){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.imageUrl = imageUrl;
+    }
+
 }
