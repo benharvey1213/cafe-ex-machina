b6d33067c1f52468f01e02c48ecf07c5caab27e8
Andy H <fan.horror@gmail.com>
2020-05-08 16:38:09 -0500

    Added category fetching on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataInteractionService } from '../data-interaction.service';
+import { MENU } from '../mock-menu'
 
 
 @Component({
@@ -11,10 +13,22 @@ export class HomeComponent implements OnInit {
 
   total : number;
 
-  constructor(private data : DataInteractionService) { }
+  constructor(private dataService : DataInteractionService, private router: Router) { }
+
+  categorySelector(category: number){
+    let categoryActual: string;
+    if(category === 1)
+      categoryActual = 'breakfast';
+    else if(category === 2)
+      categoryActual = 'panini';
+    else if(category === 3)
+      categoryActual = 'cafe drink';
+    this.dataService.currentCategory = categoryActual;
+    this.router.navigateByUrl(this.dataService.currentCategory)
+  }
 
   ngOnInit(): void {
-    this.total = this.data.getSubTotal();
+    this.total = this.dataService.getSubTotal();
   }
 
 }
